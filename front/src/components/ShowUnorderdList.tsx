e266c42cdb6e2d52a795982146ebdf7591be05fd
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2024-02-17 18:14:50 +0900

    もっときれいに

--- front/src/components/ShowUnorderdList.tsx
+++ front/src/components/ShowUnorderdList.tsx
@@ -5,32 +5,41 @@ type FAQListProps = {
     items: FAQ[];
     color: string;
     hoverColor: string;
-  };
+    input: string;
+};
 
-const FAQList = ({ items, color, hoverColor }: FAQListProps): JSX.Element => {
+const FAQList = ({ items, color, hoverColor, input }: FAQListProps): JSX.Element => {
     return (
-        <UnorderedList p={4}>
-            {items.map((faq) => (
-                <ListItem
-                    key={faq.question}
-                    pl={2}
-                    py={2}
-                    color={color}
-                    _hover={{ bg: hoverColor }}
-                    className="text-lg text-[#2B546A] list-inside list-square marker:text-[#57D5C1] rounded-md"
-                >
-                    <Link href={`/pages/${faq.pageTitle}`}>{faq.question}</Link>
-                </ListItem>
-            ))}
-        </UnorderedList>
+        <>
+            {
+                input === "" ? <span className="text-[#2B546A] text-base">
+                    Frequently Asked Questions
+                </span> : <span className="text-[#2B546A] text-base">{`${items.length} questions matched`}</span>
+
+            }
+            <UnorderedList p={4}>
+                {items.map((faq) => (
+                    <ListItem
+                        key={faq.question}
+                        pl={2}
+                        py={2}
+                        color={color}
+                        _hover={{ bg: hoverColor }}
+                        className="text-lg text-[#2B546A] list-inside list-square marker:text-[#57D5C1] rounded-md"
+                    >
+                        <Link href={`/pages/${faq.pageTitle}`}>{faq.question}</Link>
+                    </ListItem>
+                ))}
+            </UnorderedList>
+        </>
     );
 };
 
-const ShowUnorderedList = (props: { items: FAQ[] }): JSX.Element => {
-    const { items } = props;
+const ShowUnorderedList = (props: { items: FAQ[], input: string }): JSX.Element => {
+    const { items, input } = props;
     const color = useColorModeValue("#2B546A", "gray.300");
     const hoverColor = useColorModeValue("gray.200", "gray.900");
-    return <FAQList items={items} color={color} hoverColor={hoverColor} />;
+    return <FAQList items={items} color={color} hoverColor={hoverColor} input={input}/>;
 };
 
 export default ShowUnorderedList;
--- front/src/pages/top.tsx
+++ front/src/pages/top.tsx
@@ -1,6 +1,5 @@
-import wanko from "@/assets/wanko.svg";
-import prompt from "@/assets/prompt.svg";
 import ShowUnorderedList from "../components/ShowUnorderdList";
+import TopInput from "../components/TopInput";
 import useQuestion from "../hooks/useQuestion";
 
 export function TopPage(): JSX.Element {
@@ -12,41 +11,12 @@ export function TopPage(): JSX.Element {
 
   return (
     <>
-      <div className="flex flex-col">
-        <div className="flex justify-center">
-          <div className="relative w-96">
-            <img src={wanko} alt="wanko" />
-            <img
-              src={prompt}
-              alt="prompt"
-              className="absolute bottom-2 left-[4rem] md:left-[6rem]"
-            />
-          </div>
-        </div>
-        <div className="flex justify-center">
-          <input
-            type="text"
-            value={input}
-            onChange={handleInputChange}
-            placeholder="Enter the keyword"
-            data-test="search-input"
-            className="w-full sm:w-[36rem] h-12 px-4 py-3 shadow outline-0"
-          ></input>
-        </div>
-      </div>
+      <TopInput input={input} handleInputChange={handleInputChange} />
       <div className="mt-6 px-4 py-6 h-[calc(100%-12rem)] overflow-scroll shadow">
         {input === "" ? (
-          <>
-            <span className="text-[#2B546A] text-base">
-              Frequently Asked Questions
-            </span>
-            <ShowUnorderedList items={defaultFaqs} />
-          </>
+          <ShowUnorderedList items={defaultFaqs} input={input} />
         ) : (
-          <>
-            <span className="text-[#2B546A] text-base">{`${faqs.length} questions matched`}</span>
-            <ShowUnorderedList items={defaultFaqs} />
-          </>
+          <ShowUnorderedList items={faqs} input={input} />
         )}
       </div>
     </>
