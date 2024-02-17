852ee7002db55b47abaaaa32d0f443595b17ba6b
mercy34mercy <etukobamasatyan@gmail.com>
2024-02-17 20:04:06 +0900

    cd

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,15 +1,18 @@
 import fs from "fs/promises";
 import path from "path";
-import cors from "cors";
+// import cors from "cors";
 import express from "express";
 
 export const app = express();
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
-app.get("/", async (req, res) => {
-  res.status(200).json({message: "hello world!"});
-});
+// app.get("/", async (req, res) => {
+//   res.status(200).json({message: "hello world!"});
+// });
+
+// 静的ファイルの配信
+app.use(express.static(path.resolve(__dirname, "..", "..", "front", "dist")));
 
 app.get("/api/healthcheck", async (req, res) => {
   res.status(200).json({message: "ok"});
@@ -21,7 +24,7 @@ app.get("/api/faqs", async (req, res) => {
     "..",
     "..",
     "data",
-    process.env.FAQS_FILE_NAME as string,
+    "faqs.json",
   );
   const faqs = await fs.readFile(faqsPath, "utf-8");
   res.status(200).json(JSON.parse(faqs));
