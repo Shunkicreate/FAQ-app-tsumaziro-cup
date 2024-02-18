822035fae27b6c056db137cfaf7d0afb5da3942e
mercy34mercy <etukobamasatyan@gmail.com>
2024-02-18 11:46:22 +0900

    fix lint

--- front/src/components/Loading.tsx
+++ front/src/components/Loading.tsx
@@ -1,10 +1,14 @@
 import animation from "./../assets/ninzya.gif";
-import { Center, Box, Image } from "@chakra-ui/react";
+import {Center, Box, Image} from "@chakra-ui/react";
 
 const Loading = (): JSX.Element => {
   return (
-    <Center h="100vh"> {/* 画面全体をカバー */}
-      <Box boxSize="sm"> {/* GIFのサイズを調整 */}
+    <Center h="100vh">
+      {" "}
+      {/* 画面全体をカバー */}
+      <Box boxSize="sm">
+        {" "}
+        {/* GIFのサイズを調整 */}
         <Image src={animation} alt="ローディング画面" />
       </Box>
     </Center>
