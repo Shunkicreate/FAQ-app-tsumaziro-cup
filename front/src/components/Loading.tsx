6da6c56a4c0b9d0bf670bea449dc66dc7cd4e5b5
mercy34mercy <etukobamasatyan@gmail.com>
2024-02-18 11:55:25 +0900

    loading画面をいい感じに変更

--- front/src/components/Loading.tsx
+++ front/src/components/Loading.tsx
@@ -1,15 +1,16 @@
 import animation from "./../assets/ninzya.gif";
-import {Center, Box, Image} from "@chakra-ui/react";
+import { Center, Box, Image, Text } from '@chakra-ui/react';
 
 const Loading = (): JSX.Element => {
   return (
-    <Center h="100vh">
-      {" "}
-      {/* 画面全体をカバー */}
-      <Box boxSize="sm">
-        {" "}
+    <Center h="100vh"> {/* 画面全体をカバー */}
+      <Box boxSize="sm" textAlign="center"> {/* コンテンツを中央に配置 */}
         {/* GIFのサイズを調整 */}
         <Image src={animation} alt="ローディング画面" />
+        {/* 真ん中に文字を表示し、スタイルを適用 */}
+        <Text fontSize="4xl" fontWeight="bold" color="#7679B7" mb={4}>
+          Loading...
+        </Text>
       </Box>
     </Center>
   );
